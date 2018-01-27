b6d41a4f62d30d42a25b1979188a9632f3d74e6a
Emir Cem Gezer <emircem.gezer@gmail.com>
2018-01-27 23:44:30 +0300

    required packages list expanded

--- ab18ros/scripts/install-required-packages.sh
+++ ab18ros/scripts/install-required-packages.sh
@@ -4,4 +4,23 @@ sudo apt-get update
 
 sudo apt-get install -y \
   ros-kinetic-gazebo* \
-  ros-kinetic-serial 
+  ros-kinetic-serial \
+  ros-kinetic-move-base \
+  ros-kinetic-global-planner \
+  ros-kinetic-dwa-local-planner \
+  ros-kinetic-joint-state-controller \
+  ros-kinetic-joint-state-publisher \
+  ros-kinetic-diff-drive-controller \
+  ros-kinetic-controller* \
+  ros-kinetic-robot-controllers \
+  ros-kinetic-robot-pose-publisher \
+  ros-kinetic-robot-state-publisher \
+  ros-kinetic-urdf \
+  ros-kinetic-tf-conversions \
+  ros-kinetic-tf \
+  ros-kinetic-tf2 \
+  ros-kinetic-amcl \
+  ros-kinetic-rosserial-python \
+  ros-kinetic-joy-teleop \
+  ros-kinetic-rospy \
+  ros-kinetic-roscpp 
