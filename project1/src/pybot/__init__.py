a59b5ec64d941ded657d7f570a2999ab56e51433
Emir Cem Gezer <emircem.gezer@gmail.com>
2018-01-28 01:37:03 +0300

    added go to goal method

--- project1/src/pybot/__init__.py
+++ project1/src/pybot/__init__.py
@@ -4,8 +4,10 @@ from geometry_msgs.msg import Twist, Pose2D, PoseStamped
 from nav_msgs.msg import Odometry
 from sensor_msgs.msg import LaserScan
 from tf_conversions import transformations
+import math
 
-class pybot():
+
+class Pybot():
     def __init__(self, cmd_vel_topic="cmd_vel", odom_topic="odom", scan_topic="base_scan"):
         self._pose_2d = Pose2D() 
         self._pose_stamped = PoseStamped()
@@ -19,18 +21,18 @@ class pybot():
                 
 
     def set_speed(self, linear, angular):
-        t = Twist
+        t = Twist()
         t.linear.x = linear
         t.angular.z = angular
-        self._publishers["cmd_vel"].pub(t)
+        self._publishers["cmd_vel"].publish(t)
 
-    def get_pose_2d():
+    def get_pose_2d(self):
         return self._pose_2d
 
-    def get_pose_stamped():
+    def get_pose_stamped(self):
         return self._pose_stamped
 
-    def get_laser_scan():
+    def get_laser_scan(self):
         return self._laser_scan
 
     def __cb_odom(self, msg):
@@ -47,6 +49,29 @@ class pybot():
 
     def __cb_laser_scan(self, msg):
         self.__laser_scan = msg
+
+
+class Helpers():
+    def __init__():
+        pass
+
+    @staticmethod
+    def calculate_differance(point0, point1):
+        """
+        Calculates difference between two points.
+        Returns distance and angle (degrees)
+        :param point1: geometry_msgs/Point
+        :param point2: geometry_msgs/Point
+        :return: distance and angle (degrees)
+        """
+        x_diff = point1.x - point0.x
+        y_diff = point1.y - point0.y
+
+        distance = math.sqrt(x_diff ** 2 + y_diff ** 2)
+        angle = math.atan2(y_diff, x_diff)
+
+        return distance, angle
+
 
 def main():
     rospy.init_node("pybot")
