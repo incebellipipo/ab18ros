0fc22a57b530dac8d44ac0037755426a4f587072
Emir Cem Gezer <emircem.gezer@gmail.com>
2018-02-07 00:19:57 +0300

    serial comm example has finished

--- serial_comm/src/serial_comm/serial_comm.cpp
+++ serial_comm/src/serial_comm/serial_comm.cpp
@@ -2,14 +2,15 @@
 
 namespace serial_comm {
 
-  SerialComm::SerialComm(){
+  SerialComm::SerialComm() : nh_("~"){
     // default constructor
+
   }
 
-  SerialComm::SerialComm(std::string port, uint32_t baudrate)
+  SerialComm::SerialComm(std::string port, uint32_t baudrate) : nh_("~")
   {
-    serial_.setBaudrate(baudrate);
-    serial_.setPort(port);
+    baudrate_ = baudrate;
+    port_ = port;
     init();
   }
 
@@ -37,7 +38,7 @@ namespace serial_comm {
   void SerialComm::init(){
     serial_.setBaudrate(baudrate_);
     serial_.setPort(port_);
-
+    state_publisher_ = nh_.advertise<serial_comm::ArduinoState>("/arduino_state",1000);
     try{
       serial_.open();
     } catch (serial::SerialException e){
@@ -48,6 +49,7 @@ namespace serial_comm {
       return;
     }
     is_initialized_ = true;
+    ros::spinOnce();
   }
 
   std::string SerialComm::read(){
@@ -60,4 +62,39 @@ namespace serial_comm {
     return serial_.write(data) == data.size();
   }
 
-}
+  void SerialComm::run(){
+    if(!this->is_initialized_){
+      throw SerialCommException("Not initialized");
+    }
+    ros::Rate r(1000);
+    std::string res;
+    serial_comm::ArduinoState arduino_state;
+    while(ros::ok()){
+      if(serial_.available() > 0){
+        res.append(serial_.read());
+        if('\n' == res.back()){
+          std::stringstream ss(res);
+          std::vector<int> vect;
+          int i;
+          while(ss >> i){
+            vect.push_back(i);
+            if(ss.peek() == ';'){
+              ss.ignore();
+            }
+          }
+          if(vect.size() >= 6){
+            arduino_state.button_states.btn1_state = bool(vect.at(0));
+            arduino_state.button_states.btn2_state = bool(vect.at(1));
+            arduino_state.button_states.btn3_state = bool(vect.at(2));
+            arduino_state.counter_states.counter1_state = vect.at(3);
+            arduino_state.counter_states.counter2_state = vect.at(4);
+            arduino_state.counter_states.counter3_state = vect.at(5);
+            state_publisher_.publish(arduino_state);
+          }
+          res.clear();
+        }
+      }
+    }
+  }
+
+} // end namespace
