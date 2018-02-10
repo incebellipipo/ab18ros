a6d563ef43586f90956a2c8b33a4da6da60ae68c
Emir Cem Gezer <emircem.gezer@gmail.com>
2018-02-10 10:42:46 +0300

    more delicate method to install required packages

--- ab18ros/scripts/install-required-packages.sh
+++ ab18ros/scripts/install-required-packages.sh
@@ -1,27 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
-sudo apt-get update
+# sudo apt-get update
 
-sudo apt-get install -y \
-  ros-kinetic-gazebo* \
-  ros-kinetic-serial \
-  ros-kinetic-move-base \
-  ros-kinetic-global-planner \
-  ros-kinetic-dwa-local-planner \
-  ros-kinetic-joint-state-controller \
-  ros-kinetic-joint-state-publisher \
-  ros-kinetic-diff-drive-controller \
-  ros-kinetic-controller* \
-  ros-kinetic-robot-controllers \
-  ros-kinetic-robot-pose-publisher \
-  ros-kinetic-robot-state-publisher \
-  ros-kinetic-urdf \
-  ros-kinetic-tf-conversions \
-  ros-kinetic-tf \
-  ros-kinetic-tf2 \
-  ros-kinetic-amcl \
-  ros-kinetic-rosserial-python \
-  ros-kinetic-joy-teleop \
-  ros-kinetic-rospy \
-  ros-kinetic-roscpp \
-  ros-kinetic-tf2*
+ROSDISTRO=kinetic
+DISTRO=ubuntu
+CODENAME=xenial
+DIR=$(rospack find ab18ros)
+PACKAGES=$(find $DIR/.. -name package.xml | rev | cut -d/ -f2 | rev)
+for package in $PACKAGES; do 
+  echo "$(tput bold)Scanning dependencies for $package$(tput sgr0)"
+  rosdep install $package --rosdistro $ROSDISTRO --os $DISTRO:$CODENAME 
+  echo "===="
+done 
